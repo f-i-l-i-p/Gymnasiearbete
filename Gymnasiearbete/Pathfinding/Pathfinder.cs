11dbaed0d3bf3bfb8b9dee2f1901a0a9f528bf01
Filip Johnsson <Filip_Johnsson@yahoo.se>
2019-11-14 21:24:43 +0100

    Added dijkstras algorithm

--- Gymnasiearbete/Pathfinding/Pathfinder.cs
+++ Gymnasiearbete/Pathfinding/Pathfinder.cs
@@ -55,14 +55,7 @@ namespace Gymnasiearbete.Pathfinding
                 // Check if it is the destination node
                 if (n == Destination)
                 {
-                    // Create path from parent[]
-                    path = new List<int>();
-                    var currentNode = Destination;
-                    do
-                    {
-                        path.Add(currentNode);
-                        currentNode = parent[currentNode];
-                    } while (currentNode != Source);
+                    path = ConstructPathFromParents(parent);
                     return true;
                 }
 
@@ -93,85 +86,100 @@ namespace Gymnasiearbete.Pathfinding
 
         public bool Dijkstras(out List<int> path)
         {
-            var Q = new List<Vertex>();
-            for (int i = 0; i < Graph.AdjacencyList.Count; i++)
-            {
-                Q.Add(new Vertex
-                {
-                    DistToSoruce = double.PositiveInfinity,
-                    Id = i,
-                });
-            }
+            // All unvisited nodes
+            var unvisited = new List<int>();
+            for (int i = 0; i < Graph.AdjacencyList.Count; i++) unvisited.Add(i);
 
-            // Returns the index of the node with the smalest distance to the soruce
-            int IndexOfMin()
-            {
-                var minValue = Q[0].DistToSoruce;
-                int minValueIndex = 0;
+            // Distance from the node with id i to the source
+            var distance = new double[Graph.AdjacencyList.Count];
+            Populate(distance, double.PositiveInfinity);
 
-                int listLength = Q.Count;
-                for (int i = 1; i < listLength; i++)
+            // Set source node distance to 0
+            distance[Source] = 0;
+
+
+            var parent = new int[Graph.AdjacencyList.Count];
+
+
+            // Returns the index of the unvisited node with the lowest distance to the source
+            int MinDistNodeIndex()
+            {
+                var minDist = distance[unvisited[0]];
+                int minDistIndex = 0;
+
+                int unvisitedLen = unvisited.Count;
+                // Loop through all unvisited nodes
+                for (int i = 1; i < unvisitedLen; i++)
                 {
-                    if (Q[i].DistToSoruce < minValue)
+                    // distance from the node too the source
+                    var dist = distance[unvisited[i]];
+
+                    // If it is the new minimum distance
+                    if (dist < minDist)
                     {
-                        minValue = Q[i].DistToSoruce;
-                        minValueIndex = i;
+                        minDist = dist;
+                        minDistIndex = unvisited[i];
                     }
                 }
-
-                return minValueIndex;
+                return minDistIndex;
             }
 
 
-            while (Q.Count > 0)
+            while (unvisited.Count > 0)
             {
-                int index = IndexOfMin();
-                Vertex u = Q[index];
-                Q.RemoveAt(index);
-                
-                // For each neighbor of the current node
-                foreach (var e in Graph.AdjacencyList[u.Id])
+                // set the current node (u) to the node with the lowest distance from the source
+                int index = MinDistNodeIndex();
+                var u = unvisited[index];
+                // remove the node from unvisited list
+                unvisited.RemoveAt(index);
+
+                // Check if this node is the destination node
+                if (u == Destination)
+                {
+                    path = ConstructPathFromParents(parent);
+                    return true;
+                }
+
+                // For each neighbor (v) of the current node
+                foreach (var v in Graph.AdjacencyList[u])
                 {
                     // If the neigbor is unvisited
-                    if (unvisited.Contains(e))
+                    if (unvisited.Contains(v)) // TODO: Optimise the if statement
                     {
-
+                        // alt is the total distance from the source to the neighbor node.
+                        // its value will be the current nodes distacne + the edge weight from the current node to the neigbor
+                        var alt = distance[u] + 1;
+                        
+                        // If the alt distance is shorter than the nodes current distance (which will be infinite if the node is unvisited)
+                        if (alt < distance[v])
+                        {
+                            distance[v] = alt;
+                            parent[v] = u;
+                        }
                     }
                 }
             }
 
+            // no solution found
+            path = null;
+            return false;
+        }
 
-            //// All unvisited nodes
-            //var unvisited = new List<int>();
-            //for (int i = 0; i < Graph.AdjacencyList.Count; i++) unvisited.Add(i);
-
-            //// Distance from the node i to the source
-            //var distance = new double[Graph.AdjacencyList.Count];
-            //Populate(distance, double.PositiveInfinity);
-            //// Set source node distance to 0
-            //distance[Source] = 0;
-
-
-            //var parent = new int[Graph.AdjacencyList.Count];
-
-
-            //while (unvisited.Count > 0)
-            //{
-            //    // set current node to the node with the smalest distance
-            //    var u = IndexOfMin(distance);
-            //    // remove from unvisited since it now will be visited
-            //    unvisited.Remove(u);
-
-            //    // For each neighbor of the current node
-            //    foreach (var e in Graph.AdjacencyList[u])
-            //    {
-            //        // If the neigbor is unvisited
-            //        if (unvisited.Contains(e))
-            //        {
-
-            //        }
-            //    }
-            //}
+        /// <summary>
+        /// Constructs a list on node id's from the destinaation node to the source node. 
+        /// </summary>
+        /// <param name="parent">Array that contains all necessary nodes parents</param>
+        /// <returns>A list on node id's from the destinaation node to the source node.</returns>
+        private List<int> ConstructPathFromParents(int[] parent)
+        {
+            var path = new List<int>();
+            var currentNode = Destination;
+            do
+            {
+                path.Add(currentNode);
+                currentNode = parent[currentNode];
+            } while (currentNode != Source);
+            return path;
         }
 
         /// <summary>
@@ -179,44 +187,13 @@ namespace Gymnasiearbete.Pathfinding
         /// </summary>
         /// <param name="arr">The array to be populated.</param>
         /// <param name="value">Value to populate the array with.</param>
-        public static void Populate<T>(this T[] arr, T value)
+        public static void Populate<T>(T[] arr, T value)
         {
             int arrayLength = arr.Length;
             for (int i = 0; i < arrayLength; ++i)
             {
                 arr[i] = value;
             }
-        }
-
-        /// <summary>
-        /// Finds the index of the smallest number in a list.
-        /// </summary>
-        /// <typeparam name="T">Type of list objects</typeparam>
-        /// <param name="list">List to search thorugh.</param>
-        /// <returns>The intex of the smallest number.</returns>
-        public static int IndexOfMin<T>(T[] list) where T : IComparable
-        {
-            if (list == null)
-                throw new ArgumentNullException("list");
-
-            if (list.Length == 0)
-                throw new ArgumentException("List is empty.", "list");
-
-
-            var minValue = list[0];
-            int minValueIndex = 0;
-
-            int listLength = list.Length;
-            for (int i = 1; i < listLength; i++)
-            {
-                if (list[i].CompareTo(minValue) < 0)
-                {
-                    minValue = list[i];
-                    minValueIndex = i;
-                }
-            }
-
-            return minValueIndex;
         }
     }
 }
