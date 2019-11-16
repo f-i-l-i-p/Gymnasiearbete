a43ccd4ef19a40bb679382fbba11cfa1d3b4f94e
Filip Johnsson <Filip_Johnsson@yahoo.se>
2019-11-16 19:45:17 +0100

    Expanded Draw.GraphMaze to handle explored graphs

--- Gymnasiearbete/Draw.cs
+++ Gymnasiearbete/Draw.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Linq;
 
 namespace Gymnasiearbete
 {
     static class Draw
     {
-        public static void GraphMaze(Graph graph, int w, int h)
+        public static void GraphMaze(Graph graph, int w, int h, int[] path = null, int[] explored = null)
         {
             // Returns the node id at a given coordinate
             int Id(int x, int y)
@@ -12,25 +13,32 @@ namespace Gymnasiearbete
                 return x + y * w;
             }
 
+            string previousLine = null;
+
             for (int y = 0; y < h; y++)
             {
                 string line = string.Empty;
+                string nextLine = string.Empty;
 
-                // Line with nodes
+
+                // Line with nodes (nextLine)
                 for (int x = 0; x < w; x++)
                 {
-                    line += "O";
+                    if (path != null && path.Contains(Id(x, y)))
+                        nextLine += "P";
+                    else if (explored != null && explored.Contains(Id(x, y)))
+                        nextLine += "E";
+                    else
+                        nextLine += "O";
+
                     // If edge on right side
                     if (Id(x, y) + 1 < graph.AdjacencyList.Count && graph.AdjacencyList[Id(x, y)].Contains(Id(x, y) + 1))
-                        line += "---";
+                        nextLine += "---";
                     else
-                        line += "   ";
+                        nextLine += "   ";
                 }
 
-                Console.WriteLine(line);
-                line = string.Empty;
-
-                // Line without nodes (only edges)
+                // Line without nodes (only edges) (line)
                 for (int x = 0; x < w; x++)
                 {
                     // If edge under
@@ -41,8 +49,61 @@ namespace Gymnasiearbete
                     line += "   ";
                 }
 
-                Console.WriteLine(line);
+
+                // Print line without nodes (line)
+                if (previousLine != null)
+                {
+                    for (int i = 0; i < w; i += 4)
+                    {
+                        if (line[i] == ' ')
+                        {
+                            Console.Write("    ");
+                            continue;
+                        }
+
+                        if (previousLine[i] == 'P' && nextLine[i] == 'P')
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        else if (previousLine[i] == 'E' && nextLine[i] == 'E')
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                        else
+                            Console.ForegroundColor = ConsoleColor.White;
+
+                        Console.Write(line[i] + "   ");
+                    }
+                }
+                Console.WriteLine();
+
+
+                // Print line with nodes (nextLine)
+                for (int i = 0; i < w; i += 4)
+                {
+                    if (nextLine[i] == 'P')
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    else if (nextLine[i] == 'E')
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    else
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                    Console.Write(nextLine[i]);
+
+                    if (i < w - 1 && nextLine[i + 1] == '-')
+                    {
+                        if (nextLine[i] == 'P' && nextLine[i] + 4 == 'P')
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        else if (nextLine[i] == 'E' && nextLine[i] + 4 == 'E')
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write("---");
+                    }
+                    else
+                        Console.Write("   ");
+                }
+                Console.WriteLine();
+
+
+                previousLine = nextLine;
             }
+
+            Console.ResetColor();
         }
     }
 }
