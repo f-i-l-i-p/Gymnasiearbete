50806326ab7bf9f74fbfa448d0fd1ffff22fe91c
Filip Johnsson <Filip_Johnsson@yahoo.se>
2020-02-07 21:24:11 +0100

    Moved ResultSetup to new file

---
+++ Gymnasiearbete/Test/ResultSetup.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using static Gymnasiearbete.Pathfinding.GraphOptimization;
+using static Gymnasiearbete.Pathfinding.Pathfinder;
+
+namespace Gymnasiearbete.Test
+{
+    static class ResultSetup
+    {
+        public static TestResult SetupTestResult(IEnumerable<OptimizationType> optimizationTypes, IEnumerable<SearchType> searchTypes)
+        {
+            var testResult = new TestResult
+            {
+                GraphOptimizationResults = SetupGraphOptimizationResults(optimizationTypes, searchTypes),
+            };
+
+            return testResult;
+        }
+
+        private static List<GraphOptimizationResult> SetupGraphOptimizationResults(IEnumerable<OptimizationType> optimizationTypes, IEnumerable<SearchType> searchTypes)
+        {
+            var graphOptimizationResults = new List<GraphOptimizationResult>();
+
+            foreach (var optimizationType in optimizationTypes)
+            {
+                graphOptimizationResults.Add(new GraphOptimizationResult
+                {
+                    OptimizationType = optimizationType,
+                    SearchTypeResults = SetupSearchTypeResults(searchTypes),
+                });
+            }
+            return graphOptimizationResults;
+        }
+
+        private static List<SearchTypeResult> SetupSearchTypeResults(IEnumerable<SearchType> searchTypes)
+        {
+            var searchTypeResults = new List<SearchTypeResult>();
+
+            foreach (SearchType searchType in searchTypes)
+            {
+                searchTypeResults.Add(new SearchTypeResult
+                {
+                    SearchType = searchType,
+                    ComplexityResults = new List<ComplexityResult>(),
+                });
+            }
+
+            return searchTypeResults;
+        }
+    }
+}
--- Gymnasiearbete/Test/Test.cs
+++ Gymnasiearbete/Test/Test.cs
@@ -10,50 +10,6 @@ using static Gymnasiearbete.Pathfinding.Pathfinder;
 
 namespace Gymnasiearbete.Test
 {
-    static class ResultSetup
-    {
-        public static TestResult SetupTestResult(IEnumerable<OptimizationType> optimizationTypes, IEnumerable<SearchType> searchTypes)
-        {
-            var testResult = new TestResult
-            {
-                GraphOptimizationResults = SetupGraphOptimizationResults(optimizationTypes, searchTypes),
-            };
-
-            return testResult;
-        }
-
-        private static List<GraphOptimizationResult> SetupGraphOptimizationResults(IEnumerable<OptimizationType> optimizationTypes, IEnumerable<SearchType> searchTypes)
-        {
-            var graphOptimizationResults = new List<GraphOptimizationResult>();
-
-            foreach (var optimizationType in optimizationTypes)
-            {
-                graphOptimizationResults.Add(new GraphOptimizationResult
-                {
-                    OptimizationType = optimizationType,
-                    SearchTypeResults = SetupSearchTypeResults(searchTypes),
-                });
-            }
-            return graphOptimizationResults;
-        }
-
-        private static List<SearchTypeResult> SetupSearchTypeResults(IEnumerable<SearchType> searchTypes)
-        {
-            var searchTypeResults = new List<SearchTypeResult>();
-
-            foreach (SearchType searchType in searchTypes)
-            {
-                searchTypeResults.Add(new SearchTypeResult
-                {
-                    SearchType = searchType,
-                    ComplexityResults = new List<ComplexityResult>(),
-                });
-            }
-
-            return searchTypeResults;
-        }
-    }
-
     static class Test
     {
         private static int searchRepeat;
