4fc6bac643a66089ee538fd887e43e357c8ac07d
Filip Johnsson <Filip_Johnsson@yahoo.se>
2020-02-09 13:28:46 +0100

    Updated Program

--- Gymnasiearbete/Program.cs
+++ Gymnasiearbete/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Gymnasiearbete.Graphs;
+using Gymnasiearbete.MazeGeneration;
+using Gymnasiearbete.Test;
 using Gymnasiearbete.UI;
+using System;
 using System.Linq;
 using static Gymnasiearbete.Pathfinding.GraphPruning;
 using static Gymnasiearbete.Pathfinding.Pathfinder;
@@ -17,24 +20,24 @@ namespace Gymnasiearbete
                 switch (selected)
                 {
                     case 0: // Run Tests
-                        Test.TestManager.RunTests(
-                            new OptimizationType[] {OptimizationType.None, OptimizationType.CornerJumps }, 
-                            new SearchType[] { SearchType.BFS, SearchType.Dijkstras, SearchType.AStar },
+                        TestManager.RunTests(
+                            new PruningAlgorithm[] {PruningAlgorithm.None, PruningAlgorithm.CornerJumps },
+                            new PathfindingAlgorithm[] { PathfindingAlgorithm.BFS, PathfindingAlgorithm.Dijkstras, PathfindingAlgorithm.AStar },
                             10);
                         break;
                     case 1: // Regenerate Graphs
                         GraphManager.RegenerateGraphs(
                             new double[11].Select((val, index) => index * 0.1),
-                            new int[10].Select((val, index) => (index + 1) * 8),
-                            1);
+                            new int[16].Select((val, index) => (index + 1) * 8),
+                            800);
                         break;
-                    case 2:
-                        System.Console.WriteLine(0);
-                        Draw.GraphMaze(MazeGeneration.MazeGenerator.GenerateMaze(8, 0));
-                        System.Console.WriteLine(0.5);
-                        Draw.GraphMaze(MazeGeneration.MazeGenerator.GenerateMaze(8, 0.5));
-                        System.Console.WriteLine(1);
-                        Draw.GraphMaze(MazeGeneration.MazeGenerator.GenerateMaze(8, 1));
+                    case 2: // Test graph generation
+                        Console.WriteLine(0);
+                        Draw.GraphMaze(MazeGenerator.GenerateMaze(8, 0));
+                        Console.WriteLine(0.5);
+                        Draw.GraphMaze(MazeGenerator.GenerateMaze(8, 0.5));
+                        Console.WriteLine(1);
+                        Draw.GraphMaze(MazeGenerator.GenerateMaze(8, 1));
                         break;
                 }
             }
